dea1d0af93d778ddf65a2bd833d21e844b4f291c
ishimwe <ishimwehenriette5@gmail.com>
2023-06-15 01:29:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
