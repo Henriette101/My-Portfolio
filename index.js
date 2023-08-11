3654430cd090f66ecda33c76301f1c98357fa88d
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-11 22:55:12 +0530

    Change svg onclick

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "navbar") {
     x.className += " responsive";
+    document.getElementById('icon_img').src='images/menu2.png';
   } else {
+    document.getElementById('icon_img').src='svg logos/menu 2.png';
     x.className = "navbar";
   }
 }
